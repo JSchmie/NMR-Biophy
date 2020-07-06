46aadef0c769e1f71a1e32f77b94e61892490ed7
Jaikinator <schmieder.jacob@web.de>
2020-07-06 22:24:23 +0200

    Adding Text Box in lower right

--- Auswertung_NMR.py
+++ Auswertung_NMR.py
@@ -21,11 +21,13 @@ def fit_func(temp_cel, H, T_m):
     #print(counter, denominator, counter / denominator)
     return counter / denominator
 
-
+y_N_fit = np.polyfit(data[0:7, 0], data[0:7, 1], 1)
+y_U_fit = np.polyfit(data[-3:, 0], data[-3:, 1], 1)
 
 bounds = ([-np.inf, 300],[np.inf, 400])
 popt, pcov = curve_fit(fit_func, data[:,0],data[:,1], bounds = bounds )
-
+print(f"popt: {popt} \n"
+         f"pcov: {pcov}" )
 result = []
 
 for i in range(len(data[:,0])):
@@ -34,6 +36,12 @@ for i in range(len(data[:,0])):
 
 
 
-plt.plot(data[:,0], result, "-o")
-plt.plot(data[:,0], data[:,1], "-o") # Plot von Temperatur zum Meswert
+plt.plot(data[:,0], result, "-o", label= "Polynomial fit ")
+plt.plot(data[:,0], data[:,1], "-o", label=  "raw data") # Plot von Temperatur zum Meswert
+plt.text(20, 0.01, f'$Y_N$: a =  {round(y_N_fit[0],5)}\n      b = {round(y_N_fit[1],5)} \n'
+                     f'$Y_U$: a = {round(y_U_fit[0],5)}\n      b = {round(y_U_fit[1],5)} \n'
+                     f'H = {round(popt[0],2)} \n'
+                     f'$T_m = {round(popt[1],2)}$K',
+        verticalalignment='bottom', horizontalalignment='left', fontsize=12,  bbox= dict(boxstyle='round', alpha=0.1))
+plt.legend()
 plt.show()
